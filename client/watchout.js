898078beef9204b4c8a027dc08721d6ce1e04a98
nissaw <nissa.wollum@gmail.com>
2015-10-16 16:51:08 -0700

    SVG Enemies

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,16 @@
 // start slingin' some d3 here.
 
+d3.select('.arena').selectAll('.asteroid')
+  .data( [1, 2, 3, 4, 5] )
+  .enter().append('svg')
+  .attr('class', 'asteroid');
+
+// d3.selectAll('.asteroid').selectAll('img')
+//   .data( [50] )
+//   .enter()
+//   .append('img')
+//   .attr('xlink:href', 'asteroid.png')
+
+// d3.selectAll('.asteroid').data( [ 50, 50 , 50, 50, 50 ] )
+//   .attr('height', function(d) { return d + 'px'}  )
+//   .attr('width', function(d) { return d + 'px'})
